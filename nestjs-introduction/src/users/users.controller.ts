a558b7e3c20b2849a4965e26ef4865e5f2d44ac2
michaelwestblade <michaelwestblade@gmail.com>
2025-05-07 12:18:58 -0500

    users controller routes with http files

--- nestjs-introduction/src/users/users.controller.ts
+++ nestjs-introduction/src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Put,
+  Delete,
+  Param,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    return 'Hello from users';
+  }
+
+  @Post()
+  createUsers() {
+    return 'you created a user';
+  }
+
+  @Patch(':id')
+  patchUser(@Param('id') id: string) {
+    return `You patched a user with ID ${id}`;
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string) {
+    return `You put a user with ID ${id}`;
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return `You deleted a user with ID ${id}`;
+  }
+}
