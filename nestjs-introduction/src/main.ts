b981f150400c20dc43a225090e6b43f3d318f3b4
michaelwestblade <michaelwestblade@gmail.com>
2025-05-07 15:02:07 -0500

    remove any undefined props

--- nestjs-introduction/src/main.ts
+++ nestjs-introduction/src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   const port = process.env.PORT ?? 3000;
   await app.listen(port, () => {
     console.log(`App running on port ${port}`);
