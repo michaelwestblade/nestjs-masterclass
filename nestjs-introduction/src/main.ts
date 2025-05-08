04d914199ccdda926c0325c18a646b894e4962e0
michaelwestblade <michaelwestblade@gmail.com>
2025-05-08 12:57:38 -0500

    added swagger

--- nestjs-introduction/src/main.ts
+++ nestjs-introduction/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // swagger
+  const config = new DocumentBuilder().setVersion('1.0').build();
+
+  // instantiate document object
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const port = process.env.PORT ?? 3000;
   await app.listen(port, () => {
     console.log(`App running on port ${port}`);
