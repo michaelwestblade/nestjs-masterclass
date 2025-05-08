e977f6a14777ea0d8d2afbf9ea0fa3bbdbc56d76
michaelwestblade <michaelwestblade@gmail.com>
2025-05-08 11:02:15 -0500

    post service methods

--- nestjs-introduction/src/posts/posts.controller.ts
+++ nestjs-introduction/src/posts/posts.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
+import { GetPostsDto } from './dtos/get-posts.dto';
+import { GetPostDto } from './dtos/get-post.dto';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postService: PostsService) {}
+
+  @Get()
+  public getPosts(@Query() getPostsDto: GetPostsDto) {
+    return this.postService.findAll(getPostsDto);
+  }
+
+  @Get(':id')
+  public getPost(@Param() getPostDto: GetPostDto) {
+    return this.postService.findOne(getPostDto);
+  }
 }
--- nestjs-introduction/src/posts/providers/posts.service.ts
+++ nestjs-introduction/src/posts/providers/posts.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { GetPostsDto } from '../dtos/get-posts.dto';
+import { GetPostDto } from '../dtos/get-post.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  findAll(getPostsDto: GetPostsDto) {
+    return [];
+  }
+
+  findOne(getPostDto: GetPostDto) {
+    return {};
+  }
+}
