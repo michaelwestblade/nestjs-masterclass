6fc2b2b47e36ba529f763fb11dcae688f8200cb5
michaelwestblade <michaelwestblade@gmail.com>
2025-05-09 15:48:33 -0500

    create meta option route and service

--- nestjs-introduction/src/meta-options/providers/meta-options.service.ts
+++ nestjs-introduction/src/meta-options/providers/meta-options.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { MetaOptionEntity } from '../entities/meta-option.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreatePostMetaOptionsDto } from '../dtos/create-post-meta-options.dto';
 
 @Injectable()
 export class MetaOptionsService {
@@ -9,4 +10,14 @@ export class MetaOptionsService {
     @InjectRepository(MetaOptionEntity)
     private readonly metaOptionsRepository: Repository<MetaOptionEntity>,
   ) {}
+
+  async createPostMetaOptions(
+    createPostMetaOptionsDto: CreatePostMetaOptionsDto,
+  ) {
+    const metaOption = this.metaOptionsRepository.create(
+      createPostMetaOptionsDto,
+    );
+
+    return this.metaOptionsRepository.save(metaOption);
+  }
 }
