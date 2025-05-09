2ec465d8807039169142148870a8ff21451cd425
michaelwestblade <michaelwestblade@gmail.com>
2025-05-09 11:11:09 -0500

    adding typeorm

--- nestjs-introduction/src/app.module.ts
+++ nestjs-introduction/src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      username: 'admin',
+      password: 'admin',
+      database: 'nestjs-blog',
+      port: 5432,
+      synchronize: true,
+      entities: ['./src/**/*.entity{.ts,.js}'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
