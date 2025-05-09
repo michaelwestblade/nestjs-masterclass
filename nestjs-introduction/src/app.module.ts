87675794c97b34d2fc6aad02a3dea21df3b2073b
michaelwestblade <michaelwestblade@gmail.com>
2025-05-09 11:59:38 -0500

    more typeorm setup

--- nestjs-introduction/src/app.module.ts
+++ nestjs-introduction/src/app.module.ts
@@ -5,22 +5,31 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from './config/config.module';
+import * as process from 'node:process';
+
+const DB_PORT = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432;
 
 @Module({
   imports: [
     UsersModule,
     PostsModule,
     AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      username: 'admin',
-      password: 'admin',
-      database: 'nestjs-blog',
-      port: 5432,
-      synchronize: true,
-      entities: ['./src/**/*.entity{.ts,.js}'],
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST || '',
+        username: process.env.DB_USER || '',
+        password: process.env.DB_PASS || '',
+        database: process.env.DB_NAME || '',
+        port: DB_PORT,
+        synchronize: true,
+        entities: ['./src/**/*.entity{.ts,.js}'],
+      }),
     }),
+    ConfigModule,
   ],
   controllers: [AppController],
   providers: [AppService],
