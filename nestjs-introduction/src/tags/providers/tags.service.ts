b941da02fa479d1deb6051b3ee66dba80bd3b178
michaelwestblade <michaelwestblade@gmail.com>
2025-05-12 11:18:48 -0500

    connecting tag service and controller

--- nestjs-introduction/src/tags/providers/tags.service.ts
+++ nestjs-introduction/src/tags/providers/tags.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TagEntity } from '../entities/tag.entity';
 import { Repository } from 'typeorm';
+import { CreateTagDto } from '../dtos/create-tag.dto';
 
 @Injectable()
 export class TagsService {
@@ -9,4 +10,10 @@ export class TagsService {
     @InjectRepository(TagEntity)
     private readonly tagRepository: Repository<TagEntity>,
   ) {}
+
+  async create(createTagDto: CreateTagDto) {
+    const tag = await this.tagRepository.create(createTagDto);
+
+    return this.tagRepository.save(tag);
+  }
 }
--- nestjs-introduction/src/tags/tags.controller.ts
+++ nestjs-introduction/src/tags/tags.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateTagDto } from './dtos/create-tag.dto';
+import { TagsService } from './providers/tags.service';
 
 @Controller('tags')
-export class TagsController {}
+export class TagsController {
+  constructor(private readonly tagService: TagsService) {}
+
+  @Post()
+  create(@Body() createTagDto: CreateTagDto) {
+    return this.tagService.create(createTagDto);
+  }
+}
