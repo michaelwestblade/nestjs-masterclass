4408537f9aedd0c024be842abbb0e6552eb3988a
michaelwestblade <michaelwestblade@gmail.com>
2025-06-07 14:13:16 -0500

    setting up auth providers

--- nestjs-introduction/src/auth/providers/auth.service.ts
+++ nestjs-introduction/src/auth/providers/auth.service.ts
@@ -1,9 +1,4 @@
-import {
-  forwardRef,
-  Inject,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { UsersService } from '../../users/providers/users.service';
 import { SignInDto } from '../dtos/signin.dto';
 import { SignInProvider } from './sign-in.provider';
