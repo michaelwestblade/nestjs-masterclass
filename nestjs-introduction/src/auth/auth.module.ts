0ccb3102ba6be3e1fe1bc0efdb4ae84a8e162925
michaelwestblade <michaelwestblade@gmail.com>
2025-06-07 14:55:23 -0500

    generating token on signin

--- nestjs-introduction/src/auth/auth.module.ts
+++ nestjs-introduction/src/auth/auth.module.ts
@@ -5,10 +5,17 @@ import { UsersModule } from '../users/users.module';
 import { BcryptProvider } from './providers/bcrypt.provider';
 import { HashingProvider } from './providers/hashing.provider';
 import { SignInProvider } from './providers/sign-in.provider';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  imports: [forwardRef(() => UsersModule)],
+  imports: [
+    forwardRef(() => UsersModule),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   providers: [
     AuthService,
     {
--- nestjs-introduction/src/auth/providers/sign-in.provider.ts
+++ nestjs-introduction/src/auth/providers/sign-in.provider.ts
@@ -8,6 +8,9 @@ import {
 import { SignInDto } from '../dtos/signin.dto';
 import { UsersService } from '../../users/providers/users.service';
 import { HashingProvider } from './hashing.provider';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigType } from '@nestjs/config';
+import jwtConfig from '../config/jwt.config';
 
 @Injectable()
 export class SignInProvider {
@@ -15,6 +18,9 @@ export class SignInProvider {
     @Inject(forwardRef(() => UsersService))
     private readonly usersService: UsersService,
     private readonly hashingProvider: HashingProvider,
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
   ) {}
 
   public async signIn(signInDto: SignInDto) {
@@ -36,7 +42,15 @@ export class SignInProvider {
       throw new UnauthorizedException('Invalid password');
     }
 
-    const token = 'test';
+    const token = await this.jwtService.signAsync(
+      { sub: user.id, email: user.email },
+      {
+        expiresIn: this.jwtConfiguration.accessTokenTtl,
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+      },
+    );
     return token;
   }
 }
