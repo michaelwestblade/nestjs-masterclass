4e488d114659d790f060825470af6fa9f6c8cea0
michaelwestblade <michaelwestblade@gmail.com>
2025-06-08 10:10:59 -0500

    using auth guard

--- nestjs-introduction/src/users/users.controller.ts
+++ nestjs-introduction/src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   Query,
   Body,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUserDto } from './dtos/get-user.dto';
@@ -16,6 +17,7 @@ import { UsersService } from './providers/users.service';
 import { GetUsersDto } from './dtos/get-users.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateManyUsersDto } from './dtos/create-many-users.dto';
+import { AccessTokenGuard } from '../auth/guards/access-token.guard';
 
 @ApiTags('users')
 @Controller('users')
@@ -50,6 +52,7 @@ export class UsersController {
     return this.userService.create(createUserDto);
   }
 
+  @UseGuards(AccessTokenGuard)
   @Post('/create-many')
   createManyUsers(@Body() createManyUsersDto: CreateManyUsersDto) {
     return this.userService.createMany(createManyUsersDto);
