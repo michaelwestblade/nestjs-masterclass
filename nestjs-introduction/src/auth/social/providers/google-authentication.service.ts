8a461665fb91e75442f2bf4233656cbab22ffa75
michaelwestblade <michaelwestblade@gmail.com>
2025-08-02 15:15:43 -0500

    refining Google authentication flow with enhanced error handling and structured token verification

--- nestjs-introduction/src/auth/social/providers/google-authentication.service.ts
+++ nestjs-introduction/src/auth/social/providers/google-authentication.service.ts
@@ -4,6 +4,7 @@ import {
   Inject,
   Injectable,
   OnModuleInit,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { OAuth2Client } from 'google-auth-library';
 import { ConfigType } from '@nestjs/config';
@@ -38,43 +39,58 @@ export class GoogleAuthenticationService implements OnModuleInit {
     });
   }
 
+  /**
+   * Authenticates a user based on the provided Google token.
+   * Verifies the token using Google's authentication services, extracts user details,
+   * and either generates tokens for an existing user or creates a new user and generates tokens.
+   * @param {GoogleTokenDto} googleTokenDto - An object containing the Google token for authentication.
+   * @return {Promise<any>} A promise that resolves to an object containing authentication tokens if the process is successful.
+   * @throws {BadRequestException} If the provided token is invalid or missing required payload attributes.
+   * @throws {UnauthorizedException} If the authentication process fails due to invalid token or other reasons.
+   */
   public async authentication(googleTokenDto: GoogleTokenDto) {
-    // verify the token
-    const loginTicket = await this.authClient.verifyIdToken({
-      idToken: googleTokenDto.token,
-    });
-
-    // extract payload
-    const payload = loginTicket.getPayload();
-
-    if (
-      !payload ||
-      !payload.sub ||
-      !payload.email ||
-      !payload.given_name ||
-      !payload.family_name
-    ) {
-      throw new BadRequestException('Invalid token');
-    }
-
-    const { email, sub: googleId, given_name, family_name } = payload;
-
-    // find user in the db
-    const user = await this.usersService.findOneByGoogleId(googleId);
-
-    if (user) {
-      // if googleid exists, generate the token
-      return this.generateTokensProvider.generateTokens(user);
-    } else {
-      // if not create a new user and generate tokens
-      const user = await this.usersService.createGoogleUser({
-        googleId,
-        email,
-        firstName: given_name,
-        lastName: family_name,
+    try {
+      // verify the token
+      const loginTicket = await this.authClient.verifyIdToken({
+        idToken: googleTokenDto.token,
       });
-      return this.generateTokensProvider.generateTokens(user);
+
+      // extract payload
+      const payload = loginTicket.getPayload();
+
+      if (
+        !payload ||
+        !payload.sub ||
+        !payload.email ||
+        !payload.given_name ||
+        !payload.family_name
+      ) {
+        throw new BadRequestException('Invalid token');
+      }
+
+      const { email, sub: googleId, given_name, family_name } = payload;
+
+      // find user in the db
+      const user = await this.usersService.findOneByGoogleId(googleId);
+
+      if (user) {
+        // if googleid exists, generate the token
+        return this.generateTokensProvider.generateTokens(user);
+      } else {
+        // if not create a new user and generate tokens
+        const newUser = await this.usersService.createGoogleUser({
+          googleId,
+          email,
+          firstName: given_name,
+          lastName: family_name,
+        });
+
+        return this.generateTokensProvider.generateTokens(newUser);
+      }
+    } catch (error) {
+      throw new UnauthorizedException(error, {
+        description: 'Invalid token',
+      });
     }
-    // throw unauth exception
   }
 }
