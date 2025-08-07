042dedd4cc37401d3e71411523810ea0c78f5243
michaelwestblade <michaelwestblade@gmail.com>
2025-08-07 09:30:21 -0500

    add unit tests for UsersService create method and mock CreateUserProvider

--- nestjs-introduction/src/users/providers/users.service.spec.ts
+++ nestjs-introduction/src/users/providers/users.service.spec.ts
@@ -9,9 +9,24 @@ import { UsersCreateManyProvider } from './users-create-many.provider';
 import { UsersService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { UserEntity } from '../entities/user.entity';
+import { CreateUserDto } from '../dtos/create-user.dto';
 
 describe('UsersService', () => {
   let service: UsersService;
+  const mockUser = {
+    id: 'test',
+    firstName: 'test',
+    lastName: 'test',
+    email: 'test@test.net',
+    password: 'test',
+    posts: [],
+    createdAt: '',
+    updatedAt: '',
+    deletedAt: '',
+  };
+  const mockCreateUserProvider: Partial<CreateUserProvider> = {
+    create: jest.fn(() => Promise.resolve(mockUser)),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,7 +34,7 @@ describe('UsersService', () => {
         UsersService,
         { provide: DataSource, useValue: {} },
         { provide: getRepositoryToken(UserEntity), useValue: {} },
-        { provide: CreateUserProvider, useValue: {} },
+        { provide: CreateUserProvider, useValue: mockCreateUserProvider },
         { provide: UsersCreateManyProvider, useValue: {} },
         { provide: FindOneUserByEmailProvider, useValue: {} },
         { provide: FindOneByGoogleIdProvider, useValue: {} },
@@ -32,5 +47,18 @@ describe('UsersService', () => {
 
   it('Should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should be defined', () => {
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      expect(service.create).toBeDefined();
+    });
+
+    it('should create a user', async () => {
+      const user = await service.create(mockUser);
+      expect(mockCreateUserProvider.create).toHaveBeenCalled();
+      expect(user).toEqual(mockUser);
+    });
   });
 });
